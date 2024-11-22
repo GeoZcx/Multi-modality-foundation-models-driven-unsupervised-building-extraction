d00efc3931a706f2a4c5f1525f6fe5247ebda8f8
Chenxiao Zhang <42008403+GeoZcx@users.noreply.github.com>
2024-11-22 11:29:26 +0800

    Update main.py

--- codes/main.py
+++ codes/main.py
@@ -7,10 +7,8 @@ from Unsupervised_building_extraction import unsupervised_building_extraction_ch
 from Unsupervised_building_extraction import train_ch as TRAIN
 from Unsupervised_building_extraction import test_v2_ch as TEST
 import os
-from utils.metrics import Evaluator
 from PIL import Image
 import numpy as np
-
 
 def step1(img_path,building_ins_path, none_building_ins_path, cross_building_ins_path, cross_none_building_ins_path, infer_path):
     UBE.prepare_path(building_ins_path)
@@ -58,41 +56,12 @@ def step5(img_path, ckp_path, predict_path):
     b_mask = TEST.B_Masks(root_path, ckp_path, predict_path)
     b_mask.go()
 
-def evaluate_performance_of_single(infer_path, lab_path):
-    test_binary_evaluator = Evaluator(2)
-
-    pred = np.array(Image.open(infer_path))
-    gt = np.array(Image.open(lab_path))
-    if len(gt.shape) == 3:
-        gt = gt[:,:,0]
-    if len(pred.shape) == 3:
-        pred = pred[:,:,0]
-    print(pred.shape, gt.shape)
-    pred = pred / 255.0
-    gt = gt / 255.0
-
-    pred = pred[:,:gt.shape[1]]
-
-    # add the batch to the evaluator
-    test_binary_evaluator.add_batch(gt, pred)
-
-    precision, recall, F1_class, F1 = test_binary_evaluator.F1_score()
-    F1 = F1 * 100
-    F1_class = [x * 100 for x in F1_class]
-    precision = [x * 100 for x in precision]
-    recall = [x * 100 for x in recall]
-    overall_presicion = (precision[0] + precision[1]) / 2
-    overall_recall = (recall[0] + recall[1]) / 2
-    print("{:.2f} {:.2f} {:.2f} {:.2f} {:.2f} {:.2f} {:.2f} {:.2f} {:.2f}".format(F1, overall_presicion, overall_recall, F1_class[1], F1_class[0], precision[1], precision[0], recall[1], recall[0]))
-    print('+++++++++++++++++++++++++++++++++++++++')
-
-
 if __name__ == '__main__':
     abs_path = os.path.abspath(os.path.join(os.getcwd()))
     upper_path = os.path.abspath(os.path.join(abs_path, os.pardir))
 
-    img_path = 'E:\code\ohsome2label\SH_down\stockholm\img_rename'
-    tgt_path = 'E:\code\ohsome2label\SH_down\stockholm'
+    img_path = 'YOUR IMAGE PATH'
+    tgt_path = 'YOUR TARGET PATH'
     dataset_name = 'STOCKHOLM'
 
     generate_train_file_path = "{}/Unsupervised_building_extraction/data/{}".format(upper_path, dataset_name)
@@ -105,19 +74,12 @@ if __name__ == '__main__':
     train_b_path = 'E:\code\segment-anything-main\\Unsupervised_building_extraction\data\\' + dataset_name + '\\building.txt'
     train_nb_path = 'E:\code\segment-anything-main\\Unsupervised_building_extraction\data\\' + dataset_name + '\\none_building.txt'
 
-    # step1(img_path, building_ins_path, none_building_ins_path, cross_building_ins_path, cross_none_building_ins_path, predict_path)
+    step1(img_path, building_ins_path, none_building_ins_path, cross_building_ins_path, cross_none_building_ins_path, predict_path)
     #
-    # step2(tgt_path)
+    step2(tgt_path)
     #
-    # step3(tgt_path, generate_train_file_path)
+    step3(tgt_path, generate_train_file_path)
     #
-    # step4(check_path, train_b_path, train_nb_path)
+    step4(check_path, train_b_path, train_nb_path)
     #
     step5(img_path, check_path, predict_path)
-
-    # step6 evaluate
-    infer_path = predict_path + "\\predict.png"
-    lab_path = tgt_path + "\\lab_stitch\\label2.png"
-    evaluate_performance_of_single(infer_path, lab_path)
-    pseudo_label = predict_path + "\\pseudo_label.png"
-    evaluate_performance_of_single(pseudo_label, lab_path)
